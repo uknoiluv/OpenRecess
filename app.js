0f15767d0dfbd73ac344a832758df2b8359bf393
Anatoliy Ostroumov <ostroumov095@gmail.com>
2013-10-20 23:10:53 +0400

    starting mail server from main app correct port for mongo on stock configuration

--- app.js
+++ app.js
@@ -82,3 +82,6 @@ if ('development' == app.get('env')) {
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+//starting mail server
+require('./src/email.js').startServer();
